19904d77d26ab62115b73a10aee74811e044a54a
Cann7 <106887565+Cann7@users.noreply.github.com>
2022-07-07 20:57:54 -0400

    Create practicewithjoe.java

--- joepractice/practicewithjoe.java
+++ joepractice/practicewithjoe.java
@@ -3,5 +3,8 @@ package joepractice;
 public class practicewithjoe {
     public static void main(String[] args) {
         System.out.println("heyy this is a joe s practice !");
+        
+        System.out.println("heyy this is a joe s practice !");
     }
+    
 }

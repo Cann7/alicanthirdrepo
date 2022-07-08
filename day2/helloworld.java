4a57ee87926de8e50f5fc62e2c4fdc6e650a90c5
Cann7 <106887565+Cann7@users.noreply.github.com>
2022-07-07 21:23:11 -0400

    Update helloworld.java

--- day2/helloworld.java
+++ day2/helloworld.java
@@ -3,6 +3,8 @@ package day2;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello worlddd ");
+        
+        System.out.println("Hello worlddd alican welcome to world ");
 
     }
 }

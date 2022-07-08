2abe8f130e6a4c65b44630e48e165cd04b0728c8
Cann7 <106887565+Cann7@users.noreply.github.com>
2022-07-07 20:59:49 -0400

    Update practicewithjoe.java

--- joepractice/practicewithjoe.java
+++ joepractice/practicewithjoe.java
@@ -2,6 +2,9 @@ package joepractice;
 
 public class practicewithjoe {
     public static void main(String[] args) {
+        
+        System.out.println("heyy this is a joe s practice !");
+        
         System.out.println("heyy this is a joe s practice !");
     }
 }
